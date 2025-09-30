94e49d10b83bc0de5d01ac28c353f9c786be5d7a
ユーザー名 <>
2025-09-30 20:18:37 +0900

    作業三日目

---
+++ src/resources/views/component/header.blade.php
@@ -0,0 +1,32 @@
+<header class="header">
+    <div class="header__logo">
+        <a href="/">
+            <img src="{{ asset('/img/logo.svg') }}" alt="ロゴ">
+        </a>
+    </div>
+    <nav class="header__nav">
+        <ul>
+            @if(Auth::check())
+            <li>
+                <form action="/logout" method="post">
+                    @csrf
+                    <button class="header__logout">ログアウト</button>
+                </form>
+            </li>
+            <li>
+                <a href="{{ route('mypage') }}">マイページ</a>
+            </li>
+            @else
+            <li>
+                <a href="{{ route('login')) }}">ログイン</a>
+            </li>
+            <li>
+                <a href="{{ route('register') }}">会員登録</a>
+            </li>
+            @endif
+            <li class="header__btn">
+                <a href="{{ route('sell') }}">出品</a>
+            </li>
+        </ul>
+    </nav>
+</header>
---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('title','トップページ')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('/css/index.css') }}">
+@endsection
+
+@section('content')
+
+@include('components.header')
+<div class="border">
+    <ul class="border__list">
+        <li>
+            <a href="/">おすすめ</a>
+        </li>
+        <li>
+            <a href="/?tab=mylist">マイリスト</a>
+        </li>
+    </ul>
+</div>
+<div class="container">
+    <div class="items">
+        @foreach($items as $item)
+            <div class="item">
+                <a href="/item/{{ $item->id }}">
+                @if($item->sold())
+                    <div class="item__img sold">
+                        <img src="{{ Storage::url($item->img_url) }}" alt="商品画像">
+                    </div>
+                @else
+                    <div class="item__img">
+                        <img src="{{ Storage::url($item->img_url) }}" alt="商品画像">
+                    </div>
+                @endif
+                    <p class="item__name">{{$item->name}}</p>
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
+@endsection
