0612be05e53031247d05fbfbfff2541f605e176a
ユーザー名 <>
2025-10-14 21:27:20 +0900

    作業六日目

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\CategoriesTableSeeder;
+use Database\Seeders\ConditionsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(ConditionsTableSeeder::class);
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PurchaseController;
+
+use App\Http\middleware\SoldItemMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ItemController::class, 'index']);
+Route::get('/items/{item}', [ItemController::class, 'detail'])->name('items.detail');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/sell', [ItemController::class, 'sellView']);
+    Route::post('/sell', [ItemController::class, 'sellCreate']);
+    Route::post('/items/like/{item_id}/', [LikeController::class, 'create']);
+    Route::post('/items/unlike/{item_id}/', [LikeController::class, 'delete']);
+    Route::post('/items/comments/{item_id}', [CommentController::class, 'create']);
+    Route::get('/purchase/{item_id}', [PurchaseController::class, 'index'])->middleware('purchase')->name('purchase.index');
+    Route::post('/purchase/{item_id}', [PurchaseController::class, 'purchase'])->middleware('purchase');
+    Route::get('purchase/address/{item_id}', [PurchaseController::class, 'address'])->middleware('purchase')->name('purchase.address');
+    Route::post('purchase/address/{item_id}', [PurchaseController::class, 'updateAddress']);
+    Route::get('/mypage', [UserController::class, 'mypage']);
+    Route::get('/mypage/profile', [UserController::class, 'profile']);
+    Route::post('/mypage/profile', [UserController::class, 'updateProfile']);
 });
