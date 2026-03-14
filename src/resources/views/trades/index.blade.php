3aea6fd2b27acbe748ced0b34ca46ae12db068d4
ユーザー名 <>
2026-03-14 17:41:53 +0900

    細部修正

--- src/resources/views/trades/index.blade.php
+++ src/resources/views/trades/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title','取引一覧')
+
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/trades/index.css') }}">
 @endsection
--- src/resources/views/trades/show.blade.php
+++ src/resources/views/trades/show.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('title','取引チャット画面')
+
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/trades/show.css') }}">
 @endsection
